4b0fe4a49dbd0bb51f77905e484adc229c7b57ed
Chenying Zhao <chenyzh@seas.upenn.edu>
2023-05-04 21:56:12 -0400

    update yaml file according to settings in rbc

--- prep_babs-init_cmd.py
+++ prep_babs-init_cmd.py
@@ -1,18 +1,30 @@
 # This is to generate `babs-init` command
 # Used by BABS show case
 
-import os
 import os.path as op
 
-where_project = "/cbica/projects/BABS/babs_showcase"
-project_name = "showcase_HBN_fmriprep_anatonly"
-input_ds = "?????"   # path to the input dataset
-list_sub_file = None   # no pre-defined subject list file
-container_ds = "????"   # path to the container datalad dataset
-container_config_yaml_file = "????"   # path to yaml file
-container_name = "fmriprep-?-?-?"
-type_session = "single-ses"
+flag_where = "local"    # "cubic" or "local"
 
+type_session = "single-ses"
+bidsapp = "fmriprep"
+config_yaml_filename = "showcase_config_fmriprep_anatonly.yaml"
+container_name = "fmriprep-20-2-3"
+
+if flag_where == "cubic":
+    where_project = "/cbica/projects/BABS/babs_showcase"
+    input_ds = "?????"   # path to the input dataset
+    container_ds = op.join(where_project, bidsapp + "-container")
+    container_config_yaml_file = "????"   # path to yaml file
+elif flag_where == "local":
+    where_root = "/Users/chenyzh/Desktop/Research/Satterthwaite_Lab/datalad_wrapper"
+    where_project = op.join(where_root, "data")
+    # use toy data and toy container for testing only:
+    input_ds = op.join(where_project, "w2nu3")
+    container_ds = op.join(where_project, "toybidsapp-container-docker")
+    container_config_yaml_file = op.join(where_root, "babs_paper", config_yaml_filename)
+
+project_name = "showcase_HBN_fmriprep_anatonly"
+list_sub_file = None   # no pre-defined subject list file
 
 cmd = "babs-init \\\n"
 cmd += "\t" + "--where_project " + where_project + " \\\n"
@@ -25,3 +37,5 @@ cmd += "\t" + "--container_name " + container_name + " \\\n"
 cmd += "\t" + "--container_config_yaml_file " + container_config_yaml_file + " \\\n"
 cmd += "\t" + "--type_session " + type_session + " \\\n"
 cmd += "\t" + "--type_system " + "sge"
+
+print(cmd)
